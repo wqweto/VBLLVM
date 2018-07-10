65bafc5dfbe24e63994cbf9ca25a6834d4a41299
Vlad Vissoultchev <wqweto@gmail.com>
2018-07-10 19:36:46 +0300

    Enable LLVMDumpValue in release build

--- lib/build-release/setup.bat
+++ lib/build-release/setup.bat
@@ -5,7 +5,7 @@ set path=%ProgramFiles%\CMake\bin;%LOCALAPPDATA%\Programs\Python\Python36-32;%PA
 set libroot=%~dp0..
 
 if exist LLVM.sln goto :skip_gen
-cmake -G "Visual Studio 14" -D LLVM_TARGETS_TO_BUILD=X86 -D LLVM_ENABLE_RTTI=1 -D LLVM_USE_CRT_DEBUG=MTd -D LLVM_USE_CRT_RELEASE=MT -D LLVM_ENABLE_TERMINFO=OFF -D CMAKE_INSTALL_PREFIX=%libroot%\install-release %libroot%\llvm-6.0.0.src || exit /b 1
+cmake -G "Visual Studio 14" -D LLVM_TARGETS_TO_BUILD=X86 -D LLVM_ENABLE_RTTI=1 -D LLVM_USE_CRT_DEBUG=MTd -D LLVM_USE_CRT_RELEASE=MT -D LLVM_ENABLE_TERMINFO=OFF -D LLVM_ENABLE_DUMP=ON -D CMAKE_INSTALL_PREFIX=%libroot%\install-release %libroot%\llvm-6.0.0.src || exit /b 1
 :skip_gen
 :: cmake --build . --config Release || exit /b 1
 cmake --build . --config Release --target install || exit /b 1
