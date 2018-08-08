29b9202da782d85f23a63295de172a041c268f54
Vlad Vissoultchev <wqweto@gmail.com>
2018-08-08 16:19:54 +0300

    Fix typelib build

--- src/typelib/build.bat
+++ src/typelib/build.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 set prj_root=%~dp0..\..
-set gen_idl=%~dp0gen_idl\gen_idl.exe
+set gen_idl=%prj_root%\lib\gen_idl\gen_idl.exe
 set idl_file=%~dp0VBLLVM.idl
 
 %gen_idl% %prj_root%\lib\llvm-6.0.0.src\include\llvm-c -def %prj_root%\src\VBLLVM.def -o %idl_file%
-mktyplib %idl_file%
+mktyplib %idl_file% /tlb %prj_root%\bin\typelib\VBLLVM.tlb
