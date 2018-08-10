1ca53a2265d1cb48a34f2465be9d8172d09b9613
Vlad Vissoultchev <wqweto@gmail.com>
2018-08-10 16:30:45 +0300

    Regen VBLLVM.tlb

--- src/typelib/build.bat
+++ src/typelib/build.bat
@@ -4,5 +4,7 @@ set prj_root=%~dp0..\..
 set gen_idl=%prj_root%\lib\gen_idl\gen_idl.exe
 set idl_file=%~dp0VBLLVM.idl
 
+if [%1]==[tlb] goto make_tlb
 %gen_idl% %prj_root%\lib\llvm-6.0.0.src\include\llvm-c -def %prj_root%\src\VBLLVM.def -o %idl_file%
+:make_tlb
 mktyplib %idl_file% /tlb %prj_root%\bin\typelib\VBLLVM.tlb
